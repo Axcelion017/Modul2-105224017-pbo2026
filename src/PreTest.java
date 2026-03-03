c98dce70ebfb7e4bc5b815077d90309ea97f259f
Axcelion017 <michaelnewton1708@gmail.com>
2026-03-03 14:26:37 +0700

    PreTestChange

--- src/PreTest.java
+++ src/PreTest.java
@@ -1,4 +1,4 @@
-public class App {
+public class PreTest {
     public static void main(String[] args) throws Exception {
         System.out.println("=======================================================\n\n\tKARTU TANDA MAHASISWA \n\tNama: Michael Alexander Newton \n\tNIM: 105224017 \n\tProdi: Ilmu Komputer \n\n=======================================================");
     }
